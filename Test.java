5f0b8c4ccaa09a613372f553c36588cb756a377c
asteamuser <steamidzero0000@gmail.com>
2019-07-28 19:13:48 +0530

    test 1

--- Test.java
+++ Test.java
@@ -1,10 +1,21 @@
 import java.Util.Scanner;
+import java.util.regex.Pattern;
 public class Test{
   public static void main(String[] args){
-	  Scanner sc = new Scanner(System.in);
-	  System.out.print("one more line");
-	  System.out.println("aur ek line");
-	  System.out.println("chutiya Bnaying");
-	  System.out.println("adding some content");
+	  java.util.Scanner sc = new Scanner(System.in);
+	  System.out.print("Enter Your Name ");
+	  String name = sc.nextLine();
+	  System.out.println("Hello "+name+" Welcome to Our Site!!!");
+	  System.out.println("Ready of a simple Survey?")
+	  System.out.println("1.Yes");
+	  System.out.println("2.No");
+	  //int ch=Integer.parseInt(sc.nextLine());
+	  if(Integer.parseInt(sc.nextLine())==1)
+		  System.out.println("Cool!! Glad to know it "+name);
+	  else
+		  System.exit(0);
+	  System.out.println("So Which Colour Do you Like?")
+	  String colour = Pattern.matches("^[a-zA-Z]{1,9}$", sc.nextLine());//sc.nextLine();
+	  String str = new str;
   }
 }
